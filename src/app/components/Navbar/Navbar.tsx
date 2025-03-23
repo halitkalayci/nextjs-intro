d54b31cc5db0340b1a38f7a3f1a2e848c42510a1
halitklyc <halitenes.kalayci@gmail.com>
2025-03-23 16:56:34 +0300

    .

--- src/app/components/Navbar/Navbar.tsx
+++ src/app/components/Navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
@@ -13,12 +13,11 @@ import Link from "next/link";
 import { useState } from "react";
 
 export default function Navbar() {
-
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState<boolean>(false);
 
   const toggleMenu = () => {
-    setIsMobileMenuOpen(!isMobileMenuOpen)
-  }
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
 
   return (
     <nav className="border-b bg-background sticky top-0 z-50">
@@ -45,42 +44,51 @@ export default function Navbar() {
 
         {/* Mobile Navbar Button */}
         <div className="md:hidden">
-          <Button className="cursor-pointer" variant="ghost" size="icon" onClick={toggleMenu}>
-           {isMobileMenuOpen ? <X/> : <Menu />}
+          <Button
+            className="cursor-pointer"
+            variant="ghost"
+            size="icon"
+            onClick={toggleMenu}
+          >
+            {isMobileMenuOpen ? <X /> : <Menu />}
           </Button>
         </div>
         {/* Mobile Navbar Button*/}
         {/* Mobile Navbar */}
 
         {/* md:hidden -> Medium ve üstü ekranlarda hidden yani görünmez olması */}
-        <div className={"fixed inset-0 top-16 bg-background z-50 md:hidden " + (isMobileMenuOpen ? "flex flex-col" : "hidden")}>
-        <div className="flex flex-col space-y-4 p-6">
-          <Link href={"/"}>Ana Sayfa</Link>
-          <Link href={"/"}>Hakkımızda</Link>
-          <Link href={"/"}>Ürünler</Link>
+        <div
+          className={
+            "fixed inset-0 top-16 bg-background z-50 md:hidden " +
+            (isMobileMenuOpen ? "flex flex-col" : "hidden")
+          }
+        >
+          <div className="flex flex-col space-y-4 p-6">
+            <Link href={"/"}>Ana Sayfa</Link>
+            <Link href={"/"}>Hakkımızda</Link>
+            <Link href={"/"}>Ürünler</Link>
+          </div>
         </div>
-        </div>
-     
+
         {/* Mobile Navbar*/}
 
         <div className="hidden md:flex">
           <DropdownMenu>
             <DropdownMenuTrigger asChild>
-              <Button className="cursor-pointer">
-                Dil
-              </Button>
+              <Button className="cursor-pointer">Dil</Button>
             </DropdownMenuTrigger>
             <DropdownMenuContent>
               <DropdownMenuLabel>Dil Seçiniz</DropdownMenuLabel>
               <DropdownMenuSeparator />
-              <DropdownMenuItem className="cursor-pointer">Türkçe</DropdownMenuItem>
-              <DropdownMenuItem className="cursor-pointer">İngilizce</DropdownMenuItem>
+              <DropdownMenuItem className="cursor-pointer">
+                Türkçe
+              </DropdownMenuItem>
+              <DropdownMenuItem className="cursor-pointer">
+                İngilizce
+              </DropdownMenuItem>
             </DropdownMenuContent>
           </DropdownMenu>
         </div>
-
-
-
       </div>
     </nav>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,20 @@
-import Navbar from "./components/Navbar/Navbar";
+import { Card } from "@/components/ui/card";
+import Link from "next/link";
 
 export default async function Home() {
 
   return (
-    <Navbar/>
+    <>
+    <p>Ana Sayfa</p>
+    <Link href={"/csr-fetch"}>CSR Fetch Sayfası</Link>
+    {/* Güzel bir product card tasarımı component olarak projeye ekle. */}
+    <Card className="w-full max-w-xs overflow-hidden transition-all duration-300 hover:shadow-lg">
+      <div className="relative overflow-hidden">
+        <img src="https://mylifebox.com/img/blog/blog18/3.png"/>
+      </div>
+    </Card>
+    {/* Güzel bir product card tasarımı */}
+    </>
   );
 }
 
